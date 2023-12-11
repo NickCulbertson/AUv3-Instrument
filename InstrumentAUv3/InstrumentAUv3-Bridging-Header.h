979341e40cbfd62d10c2d86100b71e4b2c135619
Gary Newby <gary@mellowmuse.com>
2023-12-11 12:24:01 +0000

    Remove unused code

--- InstrumentAUv3/InstrumentAUv3-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  InstrumentAUv3-Bridging-Header.h
-//  InstrumentAUv3
-//
-//  Created by Nick Culbertson on 3/17/22.
-//
-
-#import "InstrumentAUv3DSPKernelAdapter.h"
-#import "InstrumentAUv3AudioUnit.h"
