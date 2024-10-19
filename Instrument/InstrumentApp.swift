f3419a048a8496570a6ebbc372c1ca68a3b870f5
Nick Culbertson <nickculbertson@gmail.com>
2024-10-19 14:15:27 -0400

    Update InstrumentApp.swift

--- Instrument/InstrumentApp.swift
+++ Instrument/InstrumentApp.swift
@@ -8,6 +8,18 @@ struct InstrumentApp: App {
     init() {
 #if os(iOS)
         do {
+            // Settings.sampleRate default is 44_100
+            if #available(iOS 18.0, *) {
+                if !ProcessInfo.processInfo.isMacCatalystApp && !ProcessInfo.processInfo.isiOSAppOnMac {
+                    // Set samplerRate for iOS 18 and newer (not on macOS)
+                    Settings.sampleRate = 48_000
+                }
+            }
+            if #available(macOS 15.0, *) {
+                // Set samplerRate for macOS 15 and newer
+                Settings.sampleRate = 48_000
+            }
+            
             Settings.bufferLength = .medium
             try AVAudioSession.sharedInstance().setPreferredIOBufferDuration(Settings.bufferLength.duration)
             try AVAudioSession.sharedInstance().setCategory(.playback,
